8c528adcf2279d1cbe64f41f7d89929f3265bf35
lorenzolettieri <zlettl@bbcag.ch>
2021-09-27 14:53:45 +0200

    hello world commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Dropdown, Stack} from 'react-bootstrap';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+    
+    return (
+        <Stack gap={5} className='mt-5 mx-5'>
+            <Dropdown>
+                <Dropdown.Toggle variant='primary' id='dropdown-basic'>
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => setText('Lorem impsum dolor sit amet.')}>Hoi Lena</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setText('Hello World, see ya!!')}>was lauft</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setText('Just another placeholder')}>und was sitzt?</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <h2>{text}</h2>
+        </Stack>
+    );
 }
 
 export default App;
